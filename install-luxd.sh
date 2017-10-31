757799bb183de2a4395cb7265720b9c90ae5a5b2
216k155 <32459326+216k155@users.noreply.github.com>
2017-10-31 11:17:19 +1100

    Added port config

--- install-luxd.sh
+++ install-luxd.sh
@@ -82,7 +82,7 @@ createconf() {
 	mnip=$(curl -s https://api.ipify.org)
 	rpcuser=$(date +%s | sha256sum | base64 | head -c 10 ; echo)
 	rpcpass=$(openssl rand -base64 32)
-	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=9888" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:17170" > $CONFILE
+	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "port=17170" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=9888" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:17170" > $CONFILE
 
         luxd
         message "Wait 10 seconds for daemon to load..."
@@ -93,7 +93,7 @@ createconf() {
         sleep 10s
 	sudo rm $CONFILE
 	message "Updating lux.conf..."
-        printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=9888" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:17170" > $CONFILE
+        printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "port=17170" "listen=1" "server=1" "daemon=1" "maxconnections=256" "rpcport=9888" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "masternodeaddr=$mnip:17170" > $CONFILE
 
 }
 
